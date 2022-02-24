9d5e8b99db4c2c009764504171149e2dd60f4a58
KristaOke <kristaoke@gmail.com>
2022-02-24 14:35:20 -0900

    fitting and validating more models, working out k issue

--- weight_models_all_ages.R
+++ weight_models_all_ages.R
@@ -9,6 +9,7 @@
 library(tidymv)
 library(mgcv)
 library(gamm4)
+library(cAIC4)
 
 wd <- getwd()
 lagdat <- read.csv(file=paste(wd,"/data/lagdat.csv", sep=""), row.names=1)
@@ -60,16 +61,53 @@ summary(cor_all1$mer)
 AIC(cor_all1$mer) 
 plot(cor_all1$mer)
 
+
 saveRDS(cor_all1, file=paste(wd,"/scripts/size scripts/model_output_all-ages_random-cohort.csv", sep=""))
 
+vba <- visreg(cor_all1$gam, "sst.amj", "AGE", scale="response",ylab="Partial effect on scaled log(weight-at-age)", xlab="April-June SST", rug=1,
+              data=lagdat)
+
+
+
+plot_smooths(
+  model = cor_all1$gam,
+  series = sst.amj,
+  comparison = AGE,
+  facet_terms = AGE
+) 
+
+
 #no limit on k
-cor_freek <- gamm4(log_sc_weight ~  s(sst.amj, by=AGE) + t2(LONGITUDE, LATITUDE) + s(julian),
+cor_freek <- gamm4(log_sc_weight ~  s(sst.amj, by=AGE) + t2(LONGITUDE, LATITUDE) + s(julian, k=4),
                   random=~(1|YEAR/HAUL) + (1|cohort), data=lagdat) 
 gam.check(cor_freek$gam) #is edf close to k?
 
+#double k
+dblk <- gamm4(log_sc_weight ~  s(sst.amj, by=AGE, k=8) + t2(LONGITUDE, LATITUDE) + s(julian, k=8),
+                   random=~(1|YEAR/HAUL) + (1|cohort), data=lagdat) 
+saveRDS(dblk, file=paste(wd,"/scripts/size scripts/model_output_all-ages_no-cohort_k-doubled.csv", sep=""))
 
 
+#no limit on k, no cohort
+#running without cohort quickly to look at k because cohort models 
+#are SUPER SLOW and memory intensive
+freek <- gamm4(log_sc_weight ~  s(sst.amj, by=AGE) + t2(LONGITUDE, LATITUDE) + s(julian),
+                   random=~(1|YEAR/HAUL), data=lagdat) 
+gam.check(freek$gam) #is edf close to k?
+saveRDS(freek, file=paste(wd,"/scripts/size scripts/model_output_all-ages_no-cohort_no-k-limit.csv", sep=""))
 
+#alternative: fs instead of by for large # of levles?
+freekFS <- gamm4(log_sc_weight ~  s(sst.amj, AGE, bs="fs") + t2(LONGITUDE, LATITUDE) + s(julian),
+               random=~(1|YEAR/HAUL), data=lagdat) 
+gam.check(freekFS$gam)
+
+freekml <- gamm4(log_sc_weight ~  s(sst.amj, by=AGE) + t2(LONGITUDE, LATITUDE) + s(julian),
+               random=~(1|YEAR/HAUL), data=lagdat, REML=FALSE) 
+gam.check(freekml$gam) 
+summary(freekml$gam)
+
+freekml_sel <- gamm4(log_sc_weight ~  s(sst.amj, by=AGE) + t2(LONGITUDE, LATITUDE) + s(julian),
+                 random=~(1|YEAR/HAUL), data=lagdat, REML=FALSE, select=TRUE) 
 
 #compared to no SST
 
@@ -80,3 +118,16 @@ gam.check(nosst_all1$gam)
 summary(nosst_all1$gam)
 summary(nosst_all1$mer)
 AIC(nosst_all1$mer) 
+
+
+#can we get cAIC if only og up to age 11?
+subdat11 <- lagdat[which(lagdat$AGE!="12" &
+                           lagdat$AGE!="13" & 
+                           lagdat$AGE!="14" &
+                           lagdat$AGE!="15"),]
+cor_11 <- gamm4(log_sc_weight ~  s(sst.amj, by=AGE, k=4) + t2(LONGITUDE, LATITUDE) + s(julian, k = 4),
+                  random=~(1|YEAR/HAUL) + (1|cohort), data=subdat11) 
+gam.check(cor_11$gam)
+summary(cor_11$gam)
+summary(cor_11$mer)
+cAIC(cor_11) 
