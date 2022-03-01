496aecc64cc45cc3ffb2cd4a23379c21e7b4ceee
KristaOke <kristaoke@gmail.com>
2022-03-01 14:57:43 -0900

    created a new script for models that need to be run on a cluster

---
+++ scripts/size scripts/models_for_cluster.R
@@ -0,0 +1,25 @@
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#models to run on cluster
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#Notes:
+#-these models are too big to run on Krista's computer, Mike is running them on a cluster
+#-they rely on data and models saved elsewhere
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+#packages
+library(mgcv)
+library(gamm4)
+library(cAIC4)
+
+
+
+
+
+
+
+
+
+
+
+
+
